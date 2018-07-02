5abe8fc1dcbe162af8edbb08bfe0a1589af1d268
Fabricio Loupias <loupias.fabri@gmail.com>
2018-07-02 16:19:46 -0300

    proyecto completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,20 +12,30 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatChipsModule} from '@angular/material/chips';
-
-
-
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule} from '@angular/material/menu';
+import { HttpClientModule } from '@angular/common/http';
+import { FileUploadModule } from 'ng2-file-upload';
 
 import { BeatsComponent } from './beats/beats.component';
 import { UploadBeatComponent } from './upload-beat/upload-beat.component';
 import { RegistroComponent } from './registro/registro.component';
+import { NewsComponent } from './news/news.component';
+import { BirthdayFeedComponent } from './birthday-feed/birthday-feed.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+import { FeedComponent } from './feed/feed.component';
+import { AlbumComponent } from './album/album.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
 const appRoutes: Routes = [
   { path: 'beats', component: BeatsComponent },
+  { path: 'news', component: NewsComponent },
   { path: 'upload', component: UploadBeatComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'album', component: AlbumComponent },
 ];
 
 @NgModule({
@@ -34,7 +44,12 @@ const appRoutes: Routes = [
     MyNavComponent,
     BeatsComponent,
     UploadBeatComponent,
-    RegistroComponent
+    RegistroComponent,
+    NewsComponent,
+    BirthdayFeedComponent,
+    AlbumPhotosComponent,
+    FeedComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
@@ -51,6 +66,12 @@ const appRoutes: Routes = [
     ReactiveFormsModule,
     MatInputModule,
     MatChipsModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatMenuModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    FileUploadModule
   ],
   providers: [
   ],
